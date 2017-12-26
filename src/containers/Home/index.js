8ccb82d729a451927d6adfefef0f5de34248c9cc
Patrik Marin <me@patrikmarin.fi>
2017-12-26 11:02:35 +0200

    Add resume json

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { getSiteProps } from 'react-static'
 //
-import logoImg from '../logo.png'
+import logoImg from './logo.png'
 
 export default getSiteProps(() => (
   <div>
