c6915419e2519b95e5f92ea0dfe6fa55855e0afd
Patrik Marin <me@patrikmarin.fi>
2017-12-28 20:49:36 +0200

    Add technology component to thumbnail

--- src/client/src/components/Home/ProjectComponent/ProjectEntry.tsx
+++ src/client/src/components/Home/ProjectComponent/ProjectEntry.tsx
@@ -59,9 +59,46 @@ const LinkComponent: React.SFC<{project: IProjectFields}> = (props) => {
   );
 };
 
+const TechonologyTagContainer = glamorous.div({
+  textDecoration: 'none !important',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  background: colors.white,
+  color: colors.black,
+  paddingTop: '0.5rem',
+  paddingBottom: '0.5rem',
+  borderBottom: '0.1rem solid #e1e1e1',
+  borderRadius: '0.5rem 0.5rem 0 0',
+  fontSize: '90%',
+});
+
+const TechonologyTag = glamorous.div({
+  padding: '0 0.2rem',
+  textDecoration: 'none !important',
+  display: 'inline-block',
+});
+
+const TechonologyTags: React.SFC<{tags: string[]}> = ({ tags }) => (
+  <TechonologyTagContainer>
+      {tags.map((tag: string, i: number) => {
+        return (
+          <TechonologyTag key={i}>
+            {tags.length - 1 === i ? tag : `${tag} • `}
+          </TechonologyTag>
+        );
+      })}
+  </TechonologyTagContainer>
+);
+
 const ProjectEntry: React.SFC<{project: IProjectFields}> = ({ project }) => {
+
+  const tags = ['HTML', 'CSS', 'TypeScript'];
+
   return (
     <LinkComponent project={project}>
+      <TechonologyTags tags={tags}/>
       <ProjectInfo>
         <ProjectTitle>{project.thumbnail.fields.name}</ProjectTitle>
       </ProjectInfo>
