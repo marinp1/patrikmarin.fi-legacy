ea2d0f955cf3fc9ef4000506e4e30dec33530a88
Patrik Marin <me@patrikmarin.fi>
2017-12-31 21:35:13 +0200

    Get unique tracks

--- src/client/src/apps/PlaylistMixer/Application/StatusTextComponent.tsx
+++ src/client/src/apps/PlaylistMixer/Application/StatusTextComponent.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import glamorous from 'glamorous';
 import { Playlist, Track } from './classes';
-import { timeToString } from './helpers';
+import { timeToString, getUniquetracks } from './helpers';
 
 const StatusText = glamorous.p({
   margin: 0,
@@ -9,23 +9,21 @@ const StatusText = glamorous.p({
   color: '#A1A1A1',
 });
 
-function getTotalRuntime(playlists: Playlist[]): string {
-  const runtime = Math.max(playlists.map(_ => _.runtime).reduce((a, b) => a + b, 0), 0);
+function getTotalRuntime(tracks: Track[]): string {
+  const runtime = Math.max(tracks.map(_ => _.duration).reduce((a, b) => a + b, 0), 0);
   return timeToString(runtime);
 }
 
-function getUniquetracks(playlists: Playlist[], aggressive: boolean): Track[] {
-  return [];
-}
-
 const StatusTextComponent: React.SFC<{playlists: Playlist[]}> = ({ playlists }) => {
-  const playlistString = (playlists.length === 1) ? 'playlist selected' : 'playlists selected';
+  const playlistString = (playlists.length === 1) ? ' playlist selected' : ' playlists selected';
   // Make setting for this
-  const uniqueTracks = getUniquetracks(playlists, false);
-  console.log(uniqueTracks);
-  const runtime = getTotalRuntime(playlists);
+  const uniqueTracks = getUniquetracks(playlists, true);
+  const runtime = getTotalRuntime(uniqueTracks);
   return (
-    <StatusText>{playlists.length} {playlistString} with a total runtime of {runtime}.</StatusText>
+    <StatusText>
+      {playlists.length + playlistString + ', with a total of ' +
+      uniqueTracks.length + ' unique tracks and total runtime of ' + runtime + '.'}
+    </StatusText>
   );
 };
 
--- src/client/src/apps/PlaylistMixer/Application/helpers.ts
+++ src/client/src/apps/PlaylistMixer/Application/helpers.ts
@@ -1,3 +1,5 @@
+import { Playlist, Track } from './classes';
+
 export function timeToString(time: number): string {
   const inMinutes = time / 60000;
   const inHours = inMinutes / 60;
@@ -6,4 +8,19 @@ export function timeToString(time: number): string {
   const minutes = Math.floor((inHours - hours) * 60);
   const minuteString = (minutes === 1) ? 'minute' : 'minutes';
   return `${hours} ${hourString}, ${minutes} ${minuteString}`;
+}
+
+export function getUniquetracks(playlists: Playlist[], aggressive: boolean): Track[] {
+  if (aggressive) {
+    const trackMap = new Map<string, Track>();
+    playlists.map(_ => _.tracks).reduce((a, b) => a.concat(b), []).forEach((_: Track) => {
+      if (!trackMap.get(_.artist + _.name)) trackMap.set(_.artist + _.name, _);
+    });
+    return Array.from(trackMap.values());
+  }
+  const trackMap = new Map<string, Track>();
+  playlists.map(_ => _.tracks).reduce((a, b) => a.concat(b), []).forEach((_: Track) => {
+    if (!trackMap.get(_.id)) trackMap.set(_.id, _);
+  });
+  return Array.from(trackMap.values());
 }
