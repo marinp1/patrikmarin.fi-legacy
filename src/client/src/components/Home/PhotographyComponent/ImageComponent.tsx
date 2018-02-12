59d3c994ee6ddba662d6d35765d1c21c734c2a24
Patrik Marin <me@patrikmarin.fi>
2018-02-12 18:47:35 +0100

    Add padding to image component

--- src/client/src/components/Home/PhotographyComponent/ImageComponent.tsx
+++ src/client/src/components/Home/PhotographyComponent/ImageComponent.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
+import glamorous from 'glamorous';
 
 import { IThumbnailPhoto } from './imageUtils';
+// import { colors } from '../../../styles';
+
+const ImageContainer = glamorous.div({
+  padding: '0.2rem',
+  boxSizing: 'border-box',
+  '& img': {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 const ImageComponent: React.SFC<{img: IThumbnailPhoto}> = ({ img }) => {
   if (!img.width || !img.height) return null;
-  return <img width={img.width as number} height={img.height as number} src={img.src}/>;
+  const style = { width: `${img.width as number}px`, height: `${img.height as number}px` };
+  return (
+    <ImageContainer style={style}>
+      <img src={img.src}/>
+    </ImageContainer>
+  );
 };
 
 export default ImageComponent;
