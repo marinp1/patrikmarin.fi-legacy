4656f499a314914c041911018b8b7d7a986261dc
Patrik Marin <me@patrikmarin.fi>
2018-02-13 16:19:06 +0100

    Add button for loading more images

--- src/client/src/components/Home/PhotographyComponent/GalleryComponent.tsx
+++ src/client/src/components/Home/PhotographyComponent/GalleryComponent.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import glamorous from 'glamorous';
 
-import { breakpoints } from '../../../styles';
+import { colors, breakpoints } from '../../../styles';
 import { IThumbnailPhoto, getThumbnailsWithSizes } from './imageUtils';
 import ImageComponent from './ImageComponent';
 
-const images: IThumbnailPhoto[] = [
+const demoImages: IThumbnailPhoto[] = [
   {
     src: require('./images/photo_1.jpg'),
     originalWidth: 800,
@@ -43,6 +43,15 @@ const images: IThumbnailPhoto[] = [
   },
 ];
 
+// Same width as a row of gallery images
+const LoadMoreButton = glamorous.button({
+  background: colors.lightGray,
+  color: 'black',
+  marginTop: '0.5rem',
+  marginLeft: '0.2rem',
+  width: 'calc(100% - 0.4rem)',
+});
+
 const GalleryContainer = glamorous.div({
   width: '100%',
   display: 'flex',
@@ -54,7 +63,9 @@ const GalleryContainer = glamorous.div({
 });
 
 interface IGalleryState {
-  images: IThumbnailPhoto[];
+  rowsDisplayed: number;
+  allVisible: boolean;
+  thumbnails: IThumbnailPhoto[];
 }
 
 // Gets number of elements to be displayed on a row defined by component width
@@ -64,6 +75,9 @@ function getGalleryWidth(componentWidth: number): number {
   return 4;
 }
 
+// Numbers of image rows to load per iteration
+const DEFAULT_ROW_CHUNK = 3;
+
 class GalleryComponent extends React.Component<{}, IGalleryState> {
 
   ref: HTMLDivElement | null = null;
@@ -71,18 +85,40 @@ class GalleryComponent extends React.Component<{}, IGalleryState> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      images: [],
+      rowsDisplayed: 1,
+      allVisible: false,
+      thumbnails: [],
     };
     this.refreshData = this.refreshData.bind(this);
+  }
+
+  loadImageRow() {
+    this.setState({
+      rowsDisplayed: this.state.rowsDisplayed + 1,
+    });
+  }
+
+  componentDidUpdate(nextProps: {}, nextState: IGalleryState) {
+    if (nextState.rowsDisplayed !== this.state.rowsDisplayed) {
+      this.refreshData();
+    }
   }
 
   refreshData() {
     if (!!this.ref) {
       const width = Math.floor((this.ref as HTMLDivElement).clientWidth - 1);
       const itemsPerRow = getGalleryWidth(width);
+      const imagesToBeDisplayed = this.state.rowsDisplayed * DEFAULT_ROW_CHUNK * itemsPerRow;
+
+      const demo = [...demoImages, ...demoImages, ...demoImages];
+      const images = demo.slice(0, imagesToBeDisplayed);
+
+      const allVisible = images.length >= demo.length;
+
       const thumbnails = getThumbnailsWithSizes(images, itemsPerRow, width);
       this.setState({
-        images: thumbnails,
+        thumbnails,
+        allVisible,
       });
     }
   }
@@ -100,8 +136,15 @@ class GalleryComponent extends React.Component<{}, IGalleryState> {
     return (
       <div ref={(input: HTMLDivElement) => { this.ref = input; }}>
         <GalleryContainer>
-          {this.state.images.map((img, i) => <ImageComponent key={`img-${i}`} img={img}/>)}
+          {this.state.thumbnails.map((img, i) => <ImageComponent key={`img-${i}`} img={img}/>)}
         </GalleryContainer>
+        {!this.state.allVisible &&
+          <LoadMoreButton
+            onClick={ e => this.loadImageRow() }
+          >
+            Load more images
+          </LoadMoreButton>
+         }
       </div>
     );
   }
