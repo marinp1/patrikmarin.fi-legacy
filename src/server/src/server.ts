317391764799a56d43291bbbf61a6312c376709a
Patrik Marin <me@patrikmarin.fi>
2018-02-15 12:10:07 +0100

    Reforce SSL

--- src/server/src/server.ts
+++ src/server/src/server.ts
@@ -6,7 +6,7 @@ import * as path from 'path';
 import { getContentfulClient, getProjects } from './contentful';
 import { getFlickrURL, getFlickrImages } from './flickr';
 import { getRedisClient } from './redis';
-import { getACMEChallenge } from './ssl';
+import { getACMEChallenge, forceSSL } from './ssl';
 
 export default class Server {
   private app = express();
@@ -26,11 +26,9 @@ export default class Server {
   init() {
 
     // Force SSL in production
-    /*
     if (this.isProduction) {
-      this.app.use(this.forceSSL);
+      this.app.use(forceSSL);
     }
-    */
 
     // Serve static files from the React app
     this.app.use(express.static(path.join(__dirname, '../../client/build')));
