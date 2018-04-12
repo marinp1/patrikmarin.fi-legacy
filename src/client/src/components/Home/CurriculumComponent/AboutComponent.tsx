339d068bd29593a4bb6c9717efb46253a6e798bb
Patrik Marin <patrik.marin@gmail.com>
2018-04-12 19:42:04 +0200

    Calculate age dynamically

--- src/client/src/components/Home/CurriculumComponent/AboutComponent.tsx
+++ src/client/src/components/Home/CurriculumComponent/AboutComponent.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import glamorous from 'glamorous';
+import * as moment from 'moment';
 import { colors } from '../../../styles';
 
 const AboutContainer = glamorous.div({
@@ -20,13 +21,20 @@ const Title = glamorous.h6({
   },
 });
 
+function calculateAge(): string {
+  const birthDate = moment('1995-06-08');
+  const now = moment(Date.now());
+  const age = now.diff(birthDate, 'years');
+  return String(age);
+}
+
 const AboutComponent: React.SFC<{title: string, summary: string}> = ({ title, summary }) => (
   <AboutContainer id="description" className="twelve columns">
     <Title>
       <i className="fa fa-user-circle"></i>
       {title}
     </Title>
-    <p>{summary}</p>
+    <p>{summary.replace('${age}', calculateAge)}</p>
   </AboutContainer>
 );
 
